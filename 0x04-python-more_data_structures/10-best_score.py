74e3d26fd0b860e975d6068acd6986c743e26c16
= <akinkuoroyevalentine@gmail.com>
2022-12-09 08:52:15 -0800

    Task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
+    if not a_dictionary:
+        return (None)
 
-    b_score = max(a_dictionary.values(), default=None)
-    for k, v in a_dictionary.items():
-        if v == b_score:
-            return 
+    return (max(a_dictionary, key=a_dictionary.get))
