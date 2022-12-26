0fc4e5b200ab952b7ec3974183be07c223c1b839
= <akinkuoroyevalentine@gmail.com>
2022-12-26 11:55:21 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
+
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
             count += 1
-        except IndexError:
-            pass
+
     print()
-sh: 1: wq: not found
+    return (count)
